01e219c76b0a9bdbd7abfa45d2f06938a6a2dab9
Victor <Victor@POLLY>
2016-10-03 14:49:46 +0200

    test

---
+++ main.cpp
@@ -0,0 +1,24 @@
+//#include<opencv2/core/core.hpp>
+//#include<opencv2/highgui/highgui.hpp>
+//#include<opencv2/imgproc/imgproc.hpp>
+//#include<opencv/cv.h>
+
+#include<iostream>
+#include <chrono>
+#include <time.h>
+#include <thread>
+#include <windows.h>
+#include<conio.h>           
+using namespace std;
+//using namespace cv;
+
+
+int main(int, char**)
+{
+
+	cout << "First sentence." << endl;
+	cout << "test";
+	return(0);
+
+}
+
