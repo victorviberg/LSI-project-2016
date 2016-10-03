be4897a3c98c608f2a442b2d79355f49db331060
Willi <Willi@SUPERULTRA>
2016-10-03 16:25:34 +0200

    WE

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 //#include<opencv2/core/core.hpp>
 //#include<opencv2/highgui/highgui.hpp>
 //#include<opencv2/imgproc/imgproc.hpp>
-#include <opencv/cv.h>
+//#include <opencv/cv.h>
 
 //#include "FlyCapture2.h"
 
