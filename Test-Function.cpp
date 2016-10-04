746e2208c1d4a17dea9044baf3141a718141b0ac
Victor <Victor@POLLY>
2016-10-04 17:21:20 +0200

    Added save functionality

--- Test-Function.cpp
+++ Test-Function.cpp
@@ -78,7 +78,7 @@ void CameraLoopOther()
 	cv::Mat frame; //This is a single image
 	cv::VideoCapture capture(0); //This is the "camera variable"
 	char key; //This will be used to shut down the loop, keeps track of which button is pressed
-
+	std::string filename;
 	cvNamedWindow("Camera_Output", 1); //The window we show the images in
 
 	key = 'i'; //Something else then q
@@ -88,7 +88,15 @@ void CameraLoopOther()
 		//capture >> frame; //pause/delay/waiting causes some problems which are solved by doing this twice.
 		imshow("Camera_Output", frame); //Displays the frame in our window
 	
-		key = cv::waitKey(10); //Checks for key presses and waits 10mS, if argument 0 is given it pauses until key is pressed. 
+		key = cv::waitKey(10); //Checks for key presses and waits 10mS, if argument 0 is given it pauses until key is pressed.
+		if (key == 's') //To save the image
+		{
+			cin >> filename; //Press the command window and type
+			filename = "images\\" + filename + ".png";
+			cv::imwrite(filename,frame);
+
+		}
+
 
 	}
 
@@ -121,6 +129,9 @@ void CameraLoopBW()
 		cv::Mat smallimage; //To resize the image, until we figure out how to take smaller pictures 
 		cv::resize(image, smallimage, cv::Size(800, 600), 0, 0, cv::INTER_CUBIC);
 		imshow("Camera_Output", image);
+
+
+
 	}
 
 
