fabdd005c02fcccc401680f9d61d5d3f75bf92f0
Willi <Willi@SUPERULTRA>
2016-10-09 12:56:55 +0200

    Added code in FrameClass and now it builds

--- FrameClass.cpp
+++ FrameClass.cpp
@@ -4,7 +4,9 @@ The Frame class and its functions are written here.
 #include <iostream>
 #include <list>
 #include "FrameClass.h"
+#include <opencv/cv.h>
 using namespace std;
+using namespace cv; // Alexandra: Mat is a data type in openCV, so we need to include cv here
 
 /*
 I tryed just declaring the variables and functions, but 
@@ -14,48 +16,35 @@ build the program anyway. //Camilla
 */
 
 
-// These two are just temporary because I don't know 
-// what dimensions an image matrix will have. //Camilla
-static const int Image_Height = 3;
-static const int Image_Width = 3;
-/*
-class Frame {
-private:
-	list<int> Exposure_Times;
-	list<int[Image_Height][Image_Width]> Laser_Images;
-	int Lasca_Area;
-	list<int> Dot_Position;
-	int Base_Image[Image_Height][Image_Width];
-	int Time_Averaged_Image[Image_Height][Image_Width];
-	int Contrast_Image[Image_Height][Image_Width];
-	int Perfusion_Image[Image_Height][Image_Width];
-
-public:
-	void Set_Exposure_Times(list<int>);
-	void Set_Laser_Images(list<int[Image_Height][Image_Width]>);
-	/*
-	void Set_Time_Averaged_Image();
-	Mat Set_Contrast_Image();
-	Mat Set_Perfusion_Image();
-	Mat Set_Base_Image();
-	void Set_Lasca_Area();
-	void Set_Dot_Position();
-	void Set_Base_Image();
-	Mat Get_Time_Averaged_Image();
-	Mat Get_Contrast_Image();
-	Mat Get_Perfusion_Image();
-	Mat Get_Base_Image();
-	vector<int[Image_Height][Image_Width]> Get_Laser_Images();
-	void Save_Frame();*/
-/*
-};
-*/
-/*
 void Frame::Set_Exposure_Times(list<int> Exp_Times) {
 	Exposure_Times = Exp_Times;
 }
 
-void Frame::Set_Laser_Images(list<int[Image_Height][Image_Width]> Laser_Imag) {
-	Laser_Images = Laser_Imag;
+void Frame::Set_Time_Averaged_Image(int New_Time_Averaged_Image[Image_Height][Image_Width])
+{
+	// TODO: Check if this is fast enough
+	for (int i = 0; i < Image_Height; i++)
+	{
+		for (int j = 0; j < Image_Width; j++)
+		{
+			Time_Averaged_Image[i][j] = New_Time_Averaged_Image[i][j];
+		}
+	}
 }
-*/
+
+
+
+// Alexandra: functions still to write
+
+//Mat Set_Contrast_Image();
+//Mat Set_Perfusion_Image();
+////Mat Set_Base_Image();
+//void Set_Lasca_Area();
+//void Set_Dot_Position();
+//void Set_Base_Image();
+//Mat Get_Time_Averaged_Image();
+//Mat Get_Contrast_Image();
+//Mat Get_Perfusion_Image();
+//Mat Get_Base_Image();
+//vector<int[Image_Height][Image_Width]> Get_Laser_Images();
+//void Save_Frame();
--- FrameClass.h
+++ FrameClass.h
@@ -1,12 +1,47 @@
 #pragma once
+
+#include <opencv/cv.h>
+using namespace std;
+using namespace cv; // Alexandra: Mat is a data type in openCV, so we need to include cv here
+
 /*
 The Frame class and its public functions are declared here.
 */
 
+// These two are just temporary because I don't know 
+// what dimensions an image matrix will have. //Camilla
+static const int Image_Height = 3;
+static const int Image_Width = 3;
+
+
 
 class Frame {
-/*public:
+private:
+	list<int> Exposure_Times;
+	list<int[Image_Height][Image_Width]> Laser_Images;
+	int Lasca_Area;
+	list<int> Dot_Position;
+	int Base_Image[Image_Height][Image_Width];
+	int Time_Averaged_Image[Image_Height][Image_Width];
+	int Contrast_Image[Image_Height][Image_Width];
+	int Perfusion_Image[Image_Height][Image_Width];
+public:
 	void Set_Exposure_Times(list<int>);
 	void Set_Laser_Images(list<int[Image_Height][Image_Width]>);
-*/
+
+	void Set_Time_Averaged_Image(int[Image_Height][Image_Width]);
+	Mat Set_Contrast_Image();
+	Mat Set_Perfusion_Image();
+	//Mat Set_Base_Image();
+	void Set_Lasca_Area();
+	void Set_Dot_Position();
+	void Set_Base_Image();
+	Mat Get_Time_Averaged_Image();
+	Mat Get_Contrast_Image();
+	Mat Get_Perfusion_Image();
+	Mat Get_Base_Image();
+	vector<int[Image_Height][Image_Width]> Get_Laser_Images();
+	void Save_Frame();
+
 };
+
