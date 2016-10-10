7f72e19fa3d1f16be3dfc402f9933f2e6f6e8a06
Camilla <camal940@student.liu.se>
2016-10-10 14:56:50 +0200

    Started the GUI class

--- GUIclass.h
+++ GUIclass.h
@@ -3,3 +3,22 @@
 The GUI class and its public functions are declared here.
 */
 
+#include "FrameClass.h"
+
+class GUI {
+private:
+	//vector<ROI> List_Of_ROI;
+	Frame Work_Image;
+	/*
+	Windowobject Window;
+	vector<Button> Buttons;
+
+public:
+	Void Update_GUI();
+	Void Set_Work_Image(Frame);
+
+	Void New_Event();
+	Void Disp_Perfusion_Graph();
+	Void Disp_Perfusion_Image();
+	*/
+};
