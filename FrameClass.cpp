a8c64d7fe074e2904693fcdabcc77e2dd8d2fcc7
Willi <Willi@SUPERULTRA>
2016-10-10 15:33:15 +0200

    Almost finished code and fixed some errors

--- FrameClass.cpp
+++ FrameClass.cpp
@@ -1,6 +1,7 @@
 /*
 The Frame class and its functions are written here. 
 */
+#pragma once
 #include <iostream>
 #include <list>
 #include "FrameClass.h"
@@ -8,43 +9,61 @@ The Frame class and its functions are written here.
 using namespace std;
 using namespace cv; // Alexandra: Mat is a data type in openCV, so we need to include cv here
 
-/*
-I tryed just declaring the variables and functions, but 
-got a problem with how to declare the vectors... So I
-made everything a comment so it would be possible to 
-build the program anyway. //Camilla
-*/
 
-
-void Frame::Set_Exposure_Times(list<int> Exp_Times) {
-	Exposure_Times = Exp_Times;
+// Set functions
+void Frame::Set_Exposure_Times(vector<int> New_Exposure_Times) {
+	Exposure_Times = New_Exposure_Times;
 }
 
-void Frame::Set_Time_Averaged_Image(int New_Time_Averaged_Image[Image_Height][Image_Width])
-{
-	// TODO: Check if this is fast enough
-	for (int i = 0; i < Image_Height; i++)
-	{
-		for (int j = 0; j < Image_Width; j++)
-		{
-			Time_Averaged_Image[i][j] = New_Time_Averaged_Image[i][j];
-		}
-	}
+void Frame::Set_Base_Image(Mat New_Base_Image) {
+	Base_Image = New_Base_Image;
+}
+
+void Frame::Set_Laser_Image(Mat New_Laser_Image) {
+	Laser_Image = New_Laser_Image;
+}
+
+void Frame::Set_Contrast_Image(Mat New_Contrast_Image) {
+	Contrast_Image = New_Contrast_Image;
+}
+
+void Frame::Set_Time_Averaged_Image(Mat New_Time_Averaged_Image) {
+	Time_Averaged_Image = New_Time_Averaged_Image;
+}
+
+void Frame::Set_Perfusion_Image(Mat New_Perfusion_Image) {
+	Perfusion_Image = New_Perfusion_Image;
+}
+
+void Frame::Set_Lasca_Area(int New_Lasca_Area) {
+	Lasca_Area = New_Lasca_Area;
+}
+
+void Frame::Set_Dot_Position(vector<int> New_Dot_Position) {
+	Dot_Position = New_Dot_Position;
 }
 
 
+// Get functions
+Mat Frame::Get_Base_Image() {
+	return(Base_Image);
+}
 
-// Alexandra: functions still to write
+Mat Frame::Get_Laser_Image() {
+	return(Laser_Image);
+}
 
-//Mat Set_Contrast_Image();
-//Mat Set_Perfusion_Image();
-////Mat Set_Base_Image();
-//void Set_Lasca_Area();
-//void Set_Dot_Position();
-//void Set_Base_Image();
-//Mat Get_Time_Averaged_Image();
-//Mat Get_Contrast_Image();
-//Mat Get_Perfusion_Image();
-//Mat Get_Base_Image();
-//vector<int[Image_Height][Image_Width]> Get_Laser_Images();
+Mat Frame::Get_Contrast_Image() {
+	return(Contrast_Image);
+}
+
+Mat Frame::Get_Time_Averaged_Image() {
+	return(Time_Averaged_Image);
+}
+
+Mat Frame::Get_Perfusion_Image() {
+	return(Perfusion_Image);
+}
+
+// TODO:
 //void Save_Frame();
