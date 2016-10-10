215c756e28ab3418dff0d0c4e5e6f14c8a91b905
Camilla <camal940@student.liu.se>
2016-10-10 15:48:38 +0200

    The functions of the ROI class are now written.

--- ROIclass.cpp
+++ ROIclass.cpp
@@ -1,4 +1,22 @@
 /*
 This is the ROI class and its functions.
 */
+#include "ROIclass.h"
 
+void ROI::Set_ROI_Location(vector<int> New_ROI_Location) {
+	ROI_Location = New_ROI_Location;
+}
+
+void ROI::Set_ROI_Region(Mat New_ROI_Region) {
+	ROI_Region = New_ROI_Region;
+}
+
+
+vector<int> ROI::Get_ROI_Location() {
+	return(ROI_Location);
+}
+
+
+Mat ROI::Get_ROI_Region() {
+	return(ROI_Region);
+}
--- ROIclass.h
+++ ROIclass.h
@@ -2,16 +2,21 @@
 /*
 This is the declarations of the ROI class and its public functions.
 */
+#include <iostream>
+#include <vector>
+#include <opencv/cv.h>
+using namespace std;
+using namespace cv;
+
+
 
 class ROI {
 private:
-	/*
-	Vector ROI_Location;
+	vector<int> ROI_Location;
 	Mat ROI_Region;
 public:
-	Void Set_ROI_Location();
-	Void Set_ROI_Region();
-	Vector Get_ROI_Location();
+	void Set_ROI_Location(vector<int>);
+	void Set_ROI_Region(Mat);
+	vector<int> Get_ROI_Location();
 	Mat Get_ROI_Region();
-	*/
 };
