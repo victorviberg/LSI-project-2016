456ef59f53880b982ed23c74fb39fb004d11923d
Camilla <camal940@student.liu.se>
2016-10-10 15:08:25 +0200

    Started ROI class

--- ROIclass.h
+++ ROIclass.h
@@ -3,3 +3,15 @@
 This is the declarations of the ROI class and its public functions.
 */
 
+class ROI {
+private:
+	/*
+	Vector ROI_Location;
+	Mat ROI_Region;
+public:
+	Void Set_ROI_Location();
+	Void Set_ROI_Region();
+	Vector Get_ROI_Location();
+	Mat Get_ROI_Region();
+	*/
+};
